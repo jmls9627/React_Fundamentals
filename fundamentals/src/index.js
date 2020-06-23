f901a20fb51c7ab5d1a2c9f744ba59e4f52ef69d
unknown <jmls9627@gmail.com>
2020-06-23 12:49:47 -0400

    4.Components part3

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors=[
+  {
+   name: 'Mark Twain',
+   imageUrl:'images/authors/marktwain.jpg',
+   imageSource:'Wikimedia Commons',
+   books:['the Adventures of Huckleberry Finn']
+  }
+];
+
+const state ={
+  turnData:{
+    author:authors[0],
+    books: authors[0].books
+  }
+
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <AuthorQuiz/>
+    <AuthorQuiz {...state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
