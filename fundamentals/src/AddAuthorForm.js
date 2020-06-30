07cb95d5010a88b529b39479190654d7dc6aba6d
unknown <jmls9627@gmail.com>
2020-06-30 15:28:56 -0400

    6.Form_Method onFieldChange

--- fundamentals/src/AddAuthorForm.js
+++ fundamentals/src/AddAuthorForm.js
@@ -9,16 +9,23 @@ class AuthorForm extends React.Component{
            name:'',
            imageUrl:''
        };
+       this.onFieldChange=this.onFieldChange.bind(this);
+   }
+   onFieldChange(event){
+       this.setState({
+           [event.target.name]:event.target.value
+       });
+     
    }
     render(){
   <form>
       <div className="AddAuthorForm__input">
         <label htmlFor='name'>Name</label>
-        <input type="text" name="name" value={this.state.name} />
+        <input type="text" name="name" value={this.state.name} onFieldChange={this.onFieldChange}/>
       </div>
       <div className="AddAuthorForm__input">
         <label htmlFor='imageUrl'>Image URL</label>
-        <input type="text" name="imageUrl" value={this.state.imageUrl} />
+        <input type="text" name="imageUrl" value={this.state.imageUrl} onFieldChange={this.onFieldChange}/>
       </div>
   </form>
    }
