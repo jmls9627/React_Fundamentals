12beeda86fe5f277d89230dc18325a369448d0bc
unknown <jmls9627@gmail.com>
2020-06-30 15:06:45 -0400

    6.Form_Adding CSS

--- fundamentals/src/AddAuthorForm.js
+++ fundamentals/src/AddAuthorForm.js
@@ -1,13 +1,21 @@
  import React from 'react';
+ import "./AddAuthorForm.css";
+ 
+
+
 
  
  function AddAuthorForm({match}){
-    return <div>
+    return <div className="AddAuthorForm">
      <h1>Add Author</h1>
      <form>
-         <div>
+         <div className="AddAuthorForm__input">
              <label htmlFor='name'>Name</label>
              <input type="text" name="name" />
+         </div>
+         <div className="AddAuthorForm__input">
+             <label htmlFor='imageUrl'>Image URL</label>
+             <input type="text" name="imageUrl" />
          </div>
      </form>
     </div>
