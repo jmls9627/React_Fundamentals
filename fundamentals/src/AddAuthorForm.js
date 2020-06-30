d6f1fd5623d80bc23b14ea56d90c4436c230d400
unknown <jmls9627@gmail.com>
2020-06-30 15:17:59 -0400

    6.Adding values to my form

--- fundamentals/src/AddAuthorForm.js
+++ fundamentals/src/AddAuthorForm.js
@@ -1,23 +1,36 @@
  import React from 'react';
  import "./AddAuthorForm.css";
+import { render } from 'react-dom';
  
+class AuthorForm extends React.Component{
+   constructor(props){
+       super(props);
+       this.state={
+           name:'',
+           imageUrl:''
+       };
+   }
+    render(){
+  <form>
+      <div className="AddAuthorForm__input">
+        <label htmlFor='name'>Name</label>
+        <input type="text" name="name" value={this.state.name} />
+      </div>
+      <div className="AddAuthorForm__input">
+        <label htmlFor='imageUrl'>Image URL</label>
+        <input type="text" name="imageUrl" value={this.state.imageUrl} />
+      </div>
+  </form>
+   }
+
+}
 
 
 
- 
  function AddAuthorForm({match}){
     return <div className="AddAuthorForm">
      <h1>Add Author</h1>
-     <form>
-         <div className="AddAuthorForm__input">
-             <label htmlFor='name'>Name</label>
-             <input type="text" name="name" />
-         </div>
-         <div className="AddAuthorForm__input">
-             <label htmlFor='imageUrl'>Image URL</label>
-             <input type="text" name="imageUrl" />
-         </div>
-     </form>
+     
     </div>
   }
 
